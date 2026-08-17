4e70dac5baa07a91d8f802de57317308c4bc2785
Rohullah Amin <sarwarirohullahamin6@gmail.com>
2026-08-17 15:28:11 +0430

    Day 1 variable practice

---
+++ 01-python/Day-1.py
@@ -0,0 +1,20 @@
+#name = "Rohullah Amin"
+#if name == "Rohullah Amin":
+ #   print("Hello Rohullah Amin!")
+#number = 10
+#print(number)
+#boolean_value = True
+#print(boolean_value)    
+#short_name = "Rohullah"
+#print(short_name)
+star_is = "*"
+#if name == "Rohullah Amin":
+    #print(star_is * 10)
+
+i = 0
+while i < 10:
+    print(star_is*i )
+    i += 1
+   
+    
+
